1c9f4fac1b0e39ef2924d4922e23e74901b28d2a
Iqbal Hadi Subekti <33026916+IqbalHadiSubekti@users.noreply.github.com>
2022-06-21 14:47:53 +0700

    yes
    yes

---
+++ name_cases_training.py
@@ -0,0 +1,28 @@
+print("Personal Message :")
+message = "Hello Eric, would you like to learn some Python today?"
+print(message)
+print()
+
+print("Name Cases: ")
+name = "Benetta Stefania Siagian"
+print(name.upper())
+print(name.lower())
+print(name.title())
+print()
+
+print("Famous Quote: ")
+quote = 'Albert Einstein once said,"A person who never made a mistake never tried anything new"'
+print(quote)
+print()
+
+print("Stripping Names: ")
+person_name = "Nama: \n\tIqbal\n\tHadi\n\tSubekti"
+print(person_name)
+print()
+
+print("Stripping Whitespaces: ")
+person = " Benetta Stefania Siagian "
+print(person.rstrip()) #ini untuk menghapus sebelah kanan
+print(person.lstrip()) #ini untuk menghapus sebelah kiri
+print(person.strip())  #ini untuk menghapus kanan dan kiri
+
